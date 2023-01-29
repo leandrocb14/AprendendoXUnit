7028c0524861549a6e7eef1fafb4d38a4a097e1c
Leandro Carvalho Batista <leandrocb14@gmail.com>
2023-01-29 15:24:11 -0300

    Criado modelo de negócio

---
+++ Alura.Estacionamento/Alura.Estacionamento.Modelos/TipoVeiculo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Alura.Estacionamento.Alura.Estacionamento.Modelos
+{
+    //Enumeração para classificar os veículos em carros ou motos.
+    public enum TipoVeiculo
+    {
+        Automovel = 0,
+        Motocicleta = 1
+    }
+}
+
